637a1020151d448a4cfa0b8db1b3b55b60318329
azizovmuhammadali <alico.777777@gmail.com>
2025-05-03 22:50:53 +0500

    Added and created file and words for multilanguages

---
+++ lang/en/errors.php
@@ -0,0 +1,8 @@
+<?php
+return [
+'user' => [
+    'login' => 'Your password is incorrect!',
+     'admin' => 'For administrators only',
+     'user' => 'You do not have access for it'
+],
+];
---
+++ lang/uz/errors.php
@@ -0,0 +1,8 @@
+<?php
+return [
+   'user' => [
+     'login' => 'Sizning parolingiz hato!',
+     'admin' => 'Faqat Adminlar uchun',
+     'user' => 'Sizda unga kirish huquqi yoq'
+   ],
+];

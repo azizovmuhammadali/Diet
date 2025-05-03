79c1777da63339f77fa848619f5bfd79d70f252a
azizovmuhammadali <alico.777777@gmail.com>
2025-05-03 22:46:10 +0500

    Created for user

---
+++ app/Reposities/UserReposity.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Reposities;
+
+use App\Interfaces\Reposities\UserReposityInterface;
+use App\Models\User;
+
+class UserReposity implements UserReposityInterface
+{
+   public function registerForm($data){
+  $user = new User();
+  $user->name = $data['name'];
+  $user->email = $data['email'];
+  $user->password = $data['password'];
+  $user->role = $data['status'];
+  $user->save();
+  $token = $user->createToken('auth_login')->plainTextToken;
+  return [
+   'user' => $user,
+   'token' => $token,
+];
+   }
+   public function loginForm($email){
+      return User::where('email', $email)->first();  
+  }
+  
+   public function getById($id){
+    $user = User::findOrFail($id);
+    return $user;
+   }
+   public function findById($id, $data){
+      $user = User::findOrFail($id);
+      $user->name = $data['name'] ?? $user->name;
+      $user->email = $data['email'] ?? $user->email;
+      $user->password = $data['password'] ?? $user->password;
+      $user->save();
+      return $user;
+   }
+   public function destroy($id){
+      $user = User::findOrFail($id);
+      $user->delete();
+   }
+   
+}
---
+++ app/Services/UserService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Hash;
+use App\Interfaces\Services\UserServiceInterface;
+use App\Interfaces\Reposities\UserReposityInterface;
+
+class UserService implements UserServiceInterface
+{
+    public function __construct(protected UserReposityInterface $userReposityInterface){}
+    public function register($userDTO){
+      $data = [
+        'name' => $userDTO->name,
+        'email' => $userDTO->email,
+        'password' => $userDTO->password,
+        'status' => $userDTO->status,
+      ];
+      return $this->userReposityInterface->registerForm($data);
+    }
+    public function login($data){
+        $user = $this->userReposityInterface->loginForm($data['email']);
+        if (Hash::check($data['password'], $user->password)) {
+            $token = $user->createToken('auth_login')->plainTextToken;
+            return [
+                'user' => $user,
+                'token' => $token,
+            ];
+    }
+    }
+    public function show($id){
+     return $this->userReposityInterface->getById($id);
+    }
+    public function update($id, $user){
+      $data = [
+        'name' => $user->name ?? null,
+        'email' => $user->email ?? null,
+        'password' => $user->password ?? null,
+        'status' => $user->status ?? null, 
+      ];
+      return $this->userReposityInterface->findById($id,$data);
+    }
+    public function delete($id){
+      return $this->userReposityInterface->destroy($id);
+    }
+}

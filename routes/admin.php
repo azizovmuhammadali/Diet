27b2e7bebc03b2f5f90fcba11f1475271dd2aeaf
azizovmuhammadali <alico.777777@gmail.com>
2025-05-04 20:53:41 +0500

    Added notification routes for admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\V1\api\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\api\Admin\UserController;
+use App\Http\Controllers\V1\api\Admin\ProductController;
+use App\Http\Controllers\V1\api\Admin\NotificationController;
 
 
 
@@ -12,6 +13,10 @@ use App\Http\Controllers\V1\api\Admin\UserController;
     Route::middleware(['admin', 'auth:sanctum'])->group(function () {
         Route::get('logout', [UserController::class, 'logout']);
         Route::apiResource('products',ProductController::class);
+        Route::get('notifications/unread', [NotificationController::class, 'unreadNotifications']);
+        Route::get('notifications/mark-all-as-read', [NotificationController::class, 'markAllAsRead']);
+        Route::get('notifications/all', [NotificationController::class, 'allNotifications']);
+        Route::get('markAsRead/{id}', [NotificationController::class,'markAsRead']);
     });
 
 

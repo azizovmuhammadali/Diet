db8f55f4c8cbda92332d43ac93288b5ad26f19b6
azizovmuhammadali <alico.777777@gmail.com>
2025-05-04 20:51:27 +0500

    Created order model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+  protected $fillable = [
+    'user_id',
+    'quantity',
+    'status',
+  ];
+  public function products(){
+    return $this->belongsToMany(Product::class,'order_products');
+  }
+  public function user(){
+    return $this->belongsTo(User::class);
+  }
+}

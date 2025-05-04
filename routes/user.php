6375e26d44c1249cb250d5b71dd31ad56fa929f1
azizovmuhammadali <alico.777777@gmail.com>
2025-05-04 11:27:12 +0500

    Added product routes for user

--- routes/user.php
+++ routes/user.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\V1\api\User\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\api\User\UserController;
 
@@ -11,6 +12,8 @@ use App\Http\Controllers\V1\api\User\UserController;
         Route::put('update/{id}', [UserController::class, 'update']);
         Route::delete('delete/{id}', [UserController::class, 'delete']);
         Route::get('logout', [UserController::class, 'logout']);
+        Route::get('products',[ProductController::class,'index']);
+        Route::get('products/{id}',[ProductController::class,'show']);
     });
 
 

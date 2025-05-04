36292185ee433cc2f7fd1ef64528bb962b165b79
azizovmuhammadali <alico.777777@gmail.com>
2025-05-04 11:24:06 +0500

    Created Product model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+
+class Product extends Model implements TranslatableContract
+{
+    use Translatable;
+    public $translatedAttributes = ['name', 'description'];
+    protected $fillable = [
+        'calory',
+        'image',
+        'slug',
+    ];
+    public function url(): Attribute
+    {
+        return Attribute::make(fn(): string => URL::to('storage/' . $this->image));
+    }
+}

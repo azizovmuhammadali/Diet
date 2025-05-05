3bda511886f503a025af4e21ed7fc6a949352bde
azizovmuhammadali <alico.777777@gmail.com>
2025-05-05 09:49:34 +0500

    Connected language and translation interfaces and classes

--- app/Providers/ConnectorProvider.php
+++ app/Providers/ConnectorProvider.php
@@ -2,17 +2,25 @@
 
 namespace App\Providers;
 
+use App\Interfaces\Reposities\LanguageReposityInterface;
 use App\Interfaces\Reposities\OrderReposityInterface;
 use App\Interfaces\Reposities\ProductReposityInterface;
+use App\Interfaces\Reposities\TranslationReposityInterface;
 use App\Interfaces\Reposities\UserReposityInterface;
+use App\Interfaces\Services\LanguageServiceInterface;
 use App\Interfaces\Services\OrderServiceInterface;
 use App\Interfaces\Services\ProductServiceInterface;
+use App\Interfaces\Services\TranslationServiceInterface;
 use App\Interfaces\Services\UserServiceInterface;
+use App\Reposities\LanguageReposity;
 use App\Reposities\OrderReposity;
 use App\Reposities\ProductReposity;
+use App\Reposities\TranslationReposity;
 use App\Reposities\UserReposity;
+use App\Services\LanguageService;
 use App\Services\OrderService;
 use App\Services\ProductService;
+use App\Services\TranslationService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,6 +37,10 @@ class ConnectorProvider extends ServiceProvider
         $this->app->bind(ProductServiceInterface::class,ProductService::class);
         $this->app->bind(OrderReposityInterface::class,OrderReposity::class);
         $this->app->bind(OrderServiceInterface::class,OrderService::class);
+        $this->app->bind(LanguageReposityInterface::class,LanguageReposity::class);
+        $this->app->bind(LanguageServiceInterface::class,LanguageService::class);
+        $this->app->bind(TranslationReposityInterface::class,TranslationReposity::class);
+        $this->app->bind(TranslationServiceInterface::class,TranslationService::class);
     }
 
     /**

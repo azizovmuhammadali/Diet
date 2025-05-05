1e97353bdb6c071c84ed79dbc568a467a31b28a6
azizovmuhammadali <alico.777777@gmail.com>
2025-05-05 08:02:34 +0500

    Added convertation to product

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Currency;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,6 +16,14 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         $requestedCurrency = $request->input('Currency', 'UZS');
+        $convertedPrice = $this->price;
+        if (in_array($requestedCurrency, ['RUB', 'USD'])) {
+            $currencyRate = Currency::where('Ccy', $requestedCurrency)->value('Rate');
+            if ($currencyRate) {
+                $convertedPrice = $this->price / $currencyRate;
+            }
+        }
+        $convertedPrice = round($convertedPrice, 2);
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -22,7 +31,7 @@ class ProductResource extends JsonResource
             'image' => $this->url,
             'calory' => $this->calory,
             'slug' => $this->slug,
-            'price' => $this->price,
+            'price' => $convertedPrice,
             'currency' => $requestedCurrency,
             'translations' => ProductTranslationResource::collection($this->whenLoaded('translations')),
         ];

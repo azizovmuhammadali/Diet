b50bf0133f400b13a6391765f9e9568dca40702d
azizovmuhammadali <alico.777777@gmail.com>
2025-05-06 10:07:40 +0500

    Added max Calory

--- app/DTO/OrderDTO.php
+++ app/DTO/OrderDTO.php
@@ -9,10 +9,12 @@ class OrderDTO
      */
     public $product_id;
     public $quantity;
+    public $max_calory;
 
-    public function __construct($product_id,$quantity)
+    public function __construct($product_id,$quantity,$max_calory)
     {
         $this->product_id = $product_id;
         $this->quantity = $quantity;
+        $this->max_calory = $max_calory;
     }
 }

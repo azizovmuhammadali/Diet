1aff1fb2eb7d0278f2c831f15c29ca6ced016de4
azizovmuhammadali <alico.777777@gmail.com>
2025-05-06 10:10:55 +0500

    Changed resource part because it was mistake

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Currency;
 
 class OrderResource extends JsonResource
 {
@@ -14,13 +15,58 @@ class OrderResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $requestedCurrency = $request->input('Currency', 'UZS');
+        
+        // Konvertatsiya qilingan mahsulotlar
+        $convertedProducts = $this->products->map(function($product) use ($requestedCurrency) {
+            // Mahsulotning narxini olish va konvertatsiya qilish
+            $convertedPrice = $product->price;
+            if (in_array($requestedCurrency, ['RUB', 'USD'])) {
+                $currencyRate = Currency::where('Ccy', $requestedCurrency)->value('Rate');
+                if ($currencyRate) {
+                    $convertedPrice = $product->price / $currencyRate;
+                }
+            }
+            $convertedPrice = round($convertedPrice, 2);  // 2 raqamga yaxlitlash
+            
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'description' => $product->description,
+                'image' => $product->url,
+                'calory' => $product->calory,
+                'slug' => $product->slug,
+                'price' => $convertedPrice,
+                'currency' => $requestedCurrency,
+                'quantity' => $product->pivot->quantity,  // Pivot jadvalidan miqdorni olish
+                'translations' => $product->translations->map(function($translation) {
+                    return [
+                        'id' => $translation->id,
+                        'product_id' => $translation->product_id,
+                        'locale' => $translation->locale,
+                        'name' => $translation->name,
+                        'description' => $translation->description,
+                    ];
+                }),
+            ];
+        });
+    
+        // Umumiy narxni konvertatsiya qilish
+        $convertedTotalPrice = $this->total;
+        if (in_array($requestedCurrency, ['RUB', 'USD'])) {
+            $currencyRate = Currency::where('Ccy', $requestedCurrency)->value('Rate');
+            if ($currencyRate) {
+                $convertedTotalPrice = $this->total / $currencyRate;
+            }
+        }
+        $convertedTotalPrice = round($convertedTotalPrice, 2);  // 2 raqamga yaxlitlash
+        
         return [
             'id' => $this->id,
-            'products' => ProductResource::collection($this->whenLoaded('products'))->map(function ($product) {
-                // Har bir mahsulot uchun quantityni olish
-                $product->quantity = $product->pivot->quantity;
-                return $product;
-            }),
+            'products' => $convertedProducts,
+            'total' => $convertedTotalPrice,
+            'currency' => $requestedCurrency,
         ];
     }
+    
 }
